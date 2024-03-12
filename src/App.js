27fa66c7f046187d1e786d2e0b91edb09d74aad1
abdelalimkhadija <abdelalimkhadija01@gmail.com>
2024-03-12 14:56:01 +0000

    posh it

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+function App() {
+  return (
+    <div className='flex flex-col w-full h-screen justify-center items-center bg-yellow-600'>
+
+      <div className="w-[400px] h-[200px] bg-white gap-2 text-slate-600 p-3 flex flex-col rounded-md border border-[1px] border-slate-600 shadow-lg">
+        <h1 className="text-xl">Hi to my Porfolio friends</h1>
+        <h2 className="underline">This Looks amazing isn't?</h2>
+
+        <p className="w-[20px] h-[20px] animate-bounce bg-emerald-600"></p>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
